4b5a969e12d5fa0b6020193999b1fbe1c2a2743a
Pacheca <mpacheca7@gmail.com>
2025-05-26 11:11:40 +0200

    config

---
+++ src/test/java/com/Surf/gateway_surf/GatewaySurfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Surf.gateway_surf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewaySurfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
